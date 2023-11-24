09794d07aaf5bcae63f7c5c96373647e7c5805d8
Vardan Mittal <vardanmittal000@gmail.com>
2023-11-25 00:09:34 +0530

    "Random forest model Created"

--- performace_checking.py
+++ performace_checking.py
@@ -1,10 +1,14 @@
 from sklearn.metrics import confusion_matrix, f1_score
-from sklearn.model_selection import cross_val_predict
+from sklearn.model_selection import cross_val_score
 import numpy as np
 
+def display_scores(scores):
+    print("Scores:", scores)
+    print("Mean:", scores.mean())
+    print("Standard deviation:", scores.std())
 
 def Performance_testing(model, X, y):
-    train_pred = cross_val_predict(model, X, y, cv=3)
-    matrix = confusion_matrix(y, train_pred)
-    score = f1_score(y, train_pred, average="weighted")
-    return matrix, score
+    scores = cross_val_score(model, X, y, scoring="neg_mean_squared_error", cv=10)
+    
+    rmse_scores = np.sqrt(-scores)
+    display_scores(rmse_scores)
