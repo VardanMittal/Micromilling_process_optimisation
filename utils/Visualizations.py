78e8369e5791a04773872cb7ae53f632b57e96e2
Vardan Mittal <vardanmittal000@gmail.com>
2023-11-25 18:36:11 +0530

    "Random Forest Model Completion"

--- utils/Visualizations.py
+++ utils/Visualizations.py
@@ -4,7 +4,7 @@ from subprocess import call
 
 def rf_visualize(estimator, features_name,target_name):
     export_graphviz(estimator, out_file='tree.dot', feature_names=features_name, class_names=target_name, filled=True, rounded=True)
-    with open('./images/tree.dot') as f:
+    with open('tree.dot') as f:
         dot_graph = f.read()
     graphviz.Source(dot_graph)
-    call(['dot', '-Tpng', './images/tree.dot', '-o', './images/tree.png', '-Gdpi=600'])
+    call(['dot', '-Tpng', 'tree.dot', '-o', 'tree.png', '-Gdpi=600'])
